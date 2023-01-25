7fe0c5d974b24b0f52ea9aad2ab566f03cc2ccf9
idyol <ihssandk@gmail.com>
2023-01-25 03:19:26 +0100

    add anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,59 +1,32 @@
-/* eslint-disable no-sequences */
-import { useState } from 'react'
 import './App.css';
+import { useState } from 'react';
 
-const StatisticLine = (props) =>{
-  const {text,value} = props
-  return (
-    <>
-      <tr>
-        <td>{text} </td>
-        <td>{value} </td>
-      </tr>
-    </>
-  )
-}
-
-const Button = ({action,text}) =>{
-  return <button className="item" onClick={action}>{text}</button>
-}
-
-const App = (props) => {
-  // // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const goodCount = () => setGood(good+1)
-  const neutralCount = () => setNeutral(neutral+1)
-  const badCount = () => setBad(bad+1)
-
+const App = () => {
+  const [selected, setSelected] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [votes,setVotes] = useState([0,0,0,0,0,0,0,0])
+  
   return (
     <div>
-      <div className="body">
-        <h1>give feedback</h1>
-        <div className="rate-button">
-          <Button text="good" action={goodCount} />
-          <Button text="neutral" action={neutralCount} />
-          <Button text="bad" action={badCount} />
-        </div>
-        <h1>statistics</h1>
-      {(good===0 && bad ===0 && neutral ===0)?
-            (<p>no feedback given</p>)
-         :
-        (<>
-          <table >
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={good+bad+neutral} />
-          <StatisticLine text="average" value={good-bad} />
-          <StatisticLine text="positive" value={(good)/(good+bad+neutral)*100} />
-          </table>
-        </>
-        
-        )}
-      
-      </div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]} <br/> {`has ${votes[selected]} votes`} <br/>
+      <button onClick={()=>{
+        const copy = [...votes]
+        copy[selected] +=1
+        setVotes(copy)
+        }}>vote</button>
+      <button onClick={()=>setSelected((Math.floor((Math.random())*8)))}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[(votes.indexOf(Math.max(...votes)))]}
     </div>
   )
 }
